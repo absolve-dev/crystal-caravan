a530aae415be8433a109596291224d896254759b
Angelo Lirazan <angelo.g.lirazan@gmail.com>
2015-08-06 13:40:47 -0700

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150806202818) do
+ActiveRecord::Schema.define(version: 20150806203941) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -22,6 +22,17 @@ ActiveRecord::Schema.define(version: 20150806202818) do
     t.integer  "update_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "listings", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "product_id"
+    t.decimal  "price"
+    t.integer  "quantity"
+    t.boolean  "active"
+    t.datetime "update_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
