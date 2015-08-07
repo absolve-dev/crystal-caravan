f90dff878b6a6a33ea2806b60d7534726db51bd3
insightbck <insightbck@gmail.com>
2015-08-07 22:24:54 +0000

    added seeds for a category, products, listings, line-items based on those listings, and a cart to contain the line-items.

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,25 @@ category_two = Category.create({name:'Little Man',category_id:category_one.id})
 product_one = Product.create({name:'Little Thing', category_id:category_two.id})
 
 listing_one = Listing.create({name:'Brand New',price:1.0,product_id:product_one.id})
+
+    # Seeds for Cart and LineItems
+    cart_products = Array.new
+
+    cart_listings = Array.new
+
+    cart_category = Category.create({name:'Test Category'}, category_id:10501)
+
+    for i in 0..10
+        cart_products[i] = Product.create({name:"Test Product #{i}", category_id:cart_category.id})
+        cart_listings[i] = Listing.create({name:"Test Listing #{i}", product_id:cart_products[i].id})
+    end
+
+# Cart and LineItem seeds
+
+cart_one = Cart.create({session_id:91007})
+
+line_items = Array.new
+
+for i in 0..10
+    line_items[i] = LineItem.create({listing_id:cart_listings[i].id, cart_id:cart_one.id})
+end
