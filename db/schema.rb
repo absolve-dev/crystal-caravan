92e25aaccf0e63d50ad9b066e8a964ed4016d036
insightbck <insightbck@gmail.com>
2015-08-07 20:42:53 +0000

    scaffolding generated for carts and line items. db migration performed.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150806203941) do
+ActiveRecord::Schema.define(version: 20150807201056) do
+
+  create_table "carts", force: :cascade do |t|
+    t.integer  "session_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -22,6 +28,13 @@ ActiveRecord::Schema.define(version: 20150806203941) do
     t.integer  "update_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.integer  "cart_id"
+    t.integer  "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "listings", force: :cascade do |t|
