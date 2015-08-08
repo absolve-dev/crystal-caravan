a24aa5fbd5182c677fce785ca4b9fbfb7555f040
insightbck <insightbck@gmail.com>
2015-08-08 02:41:27 +0000

    reset and migrated db to fix erroneous values from cart and lineitems.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150807210704) do
+ActiveRecord::Schema.define(version: 20150808022847) do
 
   create_table "carts", force: :cascade do |t|
     t.integer  "session_id"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20150807210704) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "listing_id"
+    t.integer  "quantity"
   end
 
   create_table "listings", force: :cascade do |t|
