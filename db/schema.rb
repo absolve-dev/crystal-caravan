1e310c4d38b42c03fc6c2d9258142825f32089f4
insightbck <insightbck@gmail.com>
2015-08-13 07:00:18 +0000

    db migration performed for 20150813015026.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150813012528) do
+ActiveRecord::Schema.define(version: 20150813015026) do
 
   create_table "carts", force: :cascade do |t|
     t.integer  "session_id"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 20150813012528) do
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.integer  "default_listing_id"
+    t.string   "default_picture"
   end
 
 end
