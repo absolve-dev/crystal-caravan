8eb1ce04a2324e9920b9d5abfb2fd74dab26764f
Angelo Lirazan <angelo.g.lirazan@gmail.com>
2015-08-21 15:57:42 -0700

    Added 'get all sets' functionality

--- app/assets/javascripts/ygo_price_api.js
+++ app/assets/javascripts/ygo_price_api.js
@@ -1,3 +1,14 @@
-//
-//
 //= require lib/angular
+var ng_ygo_update = angular.module('ng_ygo_update', []);
+
+ng_ygo_update.controller('ng_ygo_update_index', function($scope){
+  $scope.sets_array = []
+  
+  $scope.fetch_sets = function(){
+    jQuery.ajax('/ygo_price_api/sets/').done(function(data){
+      $scope.sets_array = data;
+      $scope.$apply();
+    });
+  };
+  
+});
