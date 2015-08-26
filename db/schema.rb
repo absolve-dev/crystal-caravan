fb840ed343273b2e7985097cff2de2205d488372
insightbck <insightbck@gmail.com>
2015-08-26 19:26:34 +0000

    schema update from nifty-attachments table drop. should have been included in a600b4365e813cd8af08ad4c58f0672353d78da0

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150813015026) do
+ActiveRecord::Schema.define(version: 20150822051339) do
 
   create_table "carts", force: :cascade do |t|
     t.integer  "session_id"
@@ -48,19 +48,6 @@ ActiveRecord::Schema.define(version: 20150813015026) do
     t.datetime "update_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "nifty_attachments", force: :cascade do |t|
-    t.integer  "parent_id"
-    t.string   "parent_type"
-    t.string   "token"
-    t.string   "digest"
-    t.string   "role"
-    t.string   "file_name"
-    t.string   "file_type"
-    t.binary   "data"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "products", force: :cascade do |t|
