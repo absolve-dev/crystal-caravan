c93e7d9981353725e5abd26f8fa72343681085a4
insightbck <insightbck@gmail.com>
2015-08-28 14:24:08 +0000

    rails g migration session_id_as_string. change column in cart tables from int to string.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150822051339) do
+ActiveRecord::Schema.define(version: 20150828044543) do
 
   create_table "carts", force: :cascade do |t|
-    t.integer  "session_id"
+    t.string   "session_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
