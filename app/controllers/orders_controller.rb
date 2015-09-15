73308c1fe0e946adac540659607f7f2775daa75f
Angelo Lirazan <angelo.g.lirazan@gmail.com>
2015-09-14 18:17:43 -0700

    strip orders front-end orders controller of unnecessary actions

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -18,26 +18,6 @@ class OrdersController < ApplicationController
     redirect_to :order_bill_info_form
   end
 
-  # GET /orders/1/edit
-  def edit
-  end
-
-  # POST /orders
-  # POST /orders.json
-  def create
-    @order = Order.new(order_params)
-
-    respond_to do |format|
-      if @order.save
-        format.html { redirect_to @order, notice: 'Order was successfully created.' }
-        format.json { render :show, status: :created, location: @order }
-      else
-        format.html { render :new }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   # PATCH/PUT /orders/1
   # PATCH/PUT /orders/1.json
   def update
@@ -49,16 +29,6 @@ class OrdersController < ApplicationController
         format.html { render :edit }
         format.json { render json: @order.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # DELETE /orders/1
-  # DELETE /orders/1.json
-  def destroy
-    @order.destroy
-    respond_to do |format|
-      format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
   
