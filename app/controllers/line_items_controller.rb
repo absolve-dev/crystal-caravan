e29a1fcd182a5992de2dae566986e130691fd534
Angelo Lirazan <angelo.g.lirazan@gmail.com>
2015-09-27 16:42:03 -0700

    add relevant message when line items are removed from cart, remove unnecessary code from line items controller

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,36 +1,10 @@
 class LineItemsController < ApplicationController
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
-  before_action :set_cart
   before_action :set_line_items, only: [:destroy]
-  
-  # GET /line_items
-  # GET /line_items.json
-  def index
-    @line_items = LineItem.all
-  end
-
-  # GET /line_items/1
-  # GET /line_items/1.json
-  def show
-  end
-
-  # GET /line_items/new
-  def new
-    @line_item = LineItem.new
-  end
-
-  # GET /line_items/1/edit
-  def edit
-  end
-  
-  # BCK DEFINED METHODS
-
-  #
 
   # POST /line_items
   # POST /line_items.json
   def create
-
     @line_item = LineItem.where({ :listing_id => params[:line_item][:listing_id], :cart_id => @cart.id }).first
     
     if @line_item
@@ -50,25 +24,11 @@ class LineItemsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /line_items/1
-  # PATCH/PUT /line_items/1.json
-  def update
-    respond_to do |format|
-      if @line_item.update(line_item_params)
-        format.html { redirect_to @line_item, notice: 'Line item was successfully updated.' }
-        format.json { render :show, status: :ok, location: @line_item }
-      else
-        format.html { render :edit }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   # DELETE /line_items/1
   # DELETE /line_items/1.json
   def destroy
     @line_item.destroy
-    redirect_to(show_cart_path, notice: "Listing was successfully destroyed.")
+    redirect_to(show_cart_path, notice: "'#{@line_item.listing.product.name} - #{@line_item.listing.name}' was successfully removed from your cart.")
   end
 
   private
