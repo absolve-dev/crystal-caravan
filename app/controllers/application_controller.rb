02f92cc7fb07dd67a38831ff46e781a15de1c7cd
Angelo Lirazan <angelo.g.lirazan@gmail.com>
2015-10-05 21:20:12 -0700

    add 404 when there is a routing error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActiveRecord::RecordNotFound, with: :page_not_found
+  rescue_from ActionController::RoutingError, with: :page_not_found
   
-  def record_not_found
+  def page_not_found
     redirect_to root_url, alert: "Page Not Found"
   end
   
