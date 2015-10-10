4906bd94130023eef86caae5cbcc131c6d250ef2
Angelo Lirazan <angelo.g.lirazan@gmail.com>
2015-10-10 14:28:07 -0700

    rails g mailer OrderMailer placed shipped cancelled

---
+++ app/mailers/order_mailer.rb
@@ -0,0 +1,35 @@
+class OrderMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_mailer.placed.subject
+  #
+  def placed
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_mailer.shipped.subject
+  #
+  def shipped
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_mailer.cancelled.subject
+  #
+  def cancelled
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
