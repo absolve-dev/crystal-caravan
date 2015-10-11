e796b2f6acbaddef543711ef3c23bf662e1eb608
Angelo Lirazan <angelo.g.lirazan@gmail.com>
2015-10-10 18:07:12 -0700

    add configuration for action_mailer to work with google apps email

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,21 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Configure action mailer
+Rails.application.configure do
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "gmail.com",
+    :authentication => "plain",
+    :user_name => "ruby-carbuncle@absolvegaming.com",
+    :password => "CrystalPromiseOTKorDIE",
+    :enable_starttls_auto => true
+  }
+end
