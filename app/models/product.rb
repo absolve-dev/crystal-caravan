ad05a233256336f3d6457201720ba47d77265ce3
Angelo Lirazan <angelo.g.lirazan@gmail.com>
2015-10-19 20:47:54 -0700

    remove uniqueness from product

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,7 +10,7 @@ class Product < ActiveRecord::Base
   
   has_one :catalog_card
   
-  validates :name, :presence => true, :on => :create, :uniqueness => true
+  validates :name, :presence => true, :on => :create
   validates :category_id, :presence => true
   validates :weight, :numericality => true
   validates :permalink, :presence => true, :permalink => true
